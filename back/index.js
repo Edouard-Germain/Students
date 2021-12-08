6da70436c4e3867cbf329043ddff466a9717f99a
Edouard-Germain <edangermain@gmail.com>
2021-12-08 16:17:42 +0100

    start

---
+++ back/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const port = 5000
+
+let students = [
+    {
+    name : 'Jean-Michel',
+    id : 1},
+     {
+         name: 'Jean-Kevin',
+         id: 2
+     },
+     {
+         name : 'Jean-mouloud',
+         id : 3 
+     }
+]
+
+app.get('/students',(req, res)=> {
+    res.send(students)
+})
+
+app.listen(port,()=>{
+    console.log(`app running on ${port}`)
+})
